513dc15b8aeba86954474fe643fb9f02f718dd18
coding <xueaohui_com@163.com>
2017-11-26 17:13:23 +0800

    添加nio原生的半包和粘包的处理

--- chapter1/src/main/java/com/wuhulala/newio1/ConcurrentSocketChannelUser.java
+++ chapter1/src/main/java/com/wuhulala/newio1/ConcurrentSocketChannelUser.java
@@ -19,15 +19,15 @@ public class ConcurrentSocketChannelUser {
     private SocketChannel socketChannel = null;
     private String name;
     public static void main(String[] args) throws IOException, InterruptedException {
-        for (int i = 0; i < 10; i++) {
+        //for (int i = 0; i < 10; i++) {
             new Thread(new TestClient()).start();
-        }
+        //}
     }
 
     public static class TestClient implements Runnable{
         @Override
         public void run() {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 1; i++) {
                 try {
                     new ConcurrentSocketChannelUser(Thread.currentThread().getName()+ "[" + i + "]").init();
                 } catch (IOException e) {
@@ -56,13 +56,33 @@ public class ConcurrentSocketChannelUser {
         }
 
         //------------ 注册自己的存在------------------
-        String newData = name;
-        ByteBuffer buf = ByteBuffer.allocate(480);
-        buf.clear();
-        buf.put(newData.getBytes());
-        buf.flip();
-        socketChannel.write(buf);
-        buf.clear();
+        String newData = "";
+        //测试 粘包问题,可以通过/r/n后台拆分
+        for (int i = 0; i < 50; i++) {
+            newData = name + "\r\n";
+            socketChannel.write(UTF8Utils.encode(newData));
+
+        }
+        //这样服务器此时收到了50条命令
+
+        // 模拟拆包  完整的一条命令分为50次发送和一次分割符的发送
+        for (int i = 0; i < 50; i++) {
+            newData = name;
+            socketChannel.write(UTF8Utils.encode(newData));
+        }
+        socketChannel.write(UTF8Utils.encode("\r\n"));
+        //这样服务器此时应该又收到一条命令，加上上面的总共有51条命令
+//        ByteBuffer buf = ByteBuffer.allocate(5000 * name.getBytes().length);
+//        buf.clear();
+//        buf.put(newData.getBytes());
+//        buf.flip();
+        try {
+            System.out.println("我先休息十秒再发");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //buf.clear();
         socketChannel.write(UTF8Utils.encode("lisi:nihao"));
 
         //-----------打开自己的读取功能----------------
@@ -86,7 +106,9 @@ public class ConcurrentSocketChannelUser {
                             while (sc.read(buff) > 0) {
                                 buff.flip();
                                 content += UTF8Utils.decode(buff);
+                                buff.clear();
                             }
+                            System.out.println("聊天信息大小：" + content.length());
                             System.out.println("聊天信息：" + content);
                         }
                     }
--- chapter1/src/main/java/com/wuhulala/newio1/ServerSocketChannelUser.java
+++ chapter1/src/main/java/com/wuhulala/newio1/ServerSocketChannelUser.java
@@ -7,7 +7,11 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -20,6 +24,11 @@ public class ServerSocketChannelUser {
 
     private static Map<String, SocketAddress> nameAddress = new ConcurrentHashMap<>();
     private static Map<SocketAddress, SocketChannel> addressChannel = new ConcurrentHashMap<>();
+    private static Map<SocketAddress, Integer> addressReqCount = new ConcurrentHashMap<>();
+    /**
+     * 用来存取本次多读取了多余的半包
+      */
+    private static Map<SocketAddress, Object> addressOldReq = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
         ServerSocketChannel ssc = null;
@@ -55,6 +64,7 @@ public class ServerSocketChannelUser {
                         SocketChannel sc = ssc.accept();
                         sc.configureBlocking(false);
                         try {
+                            System.out.println("注册地址为[" + sc.getRemoteAddress() + "]的客户端");
                             sc.register(selector, SelectionKey.OP_READ);
                         } catch (ClosedChannelException e) {
                             System.out.println("客户端关闭了：" + e.getMessage());
@@ -83,52 +93,142 @@ public class ServerSocketChannelUser {
     }
 
     private static String translate(int i) {
-        return null;
+        if (i == OP_ACCEPT) {
+            return "服务端可以接收这个客户端连接了";
+        } else if (i == OP_CONNECT) {
+            return "客户端已经与服务器连接成功";
+        } else if (i == OP_READ) {
+            return "可以从通道中读取数据了";
+        } else if (i == OP_WRITE) {
+            return "可以向通道中写入数据了";
+        } else {
+            return "未知事件";
+        }
     }
 
+    public static final int OP_READ = 1 << 0;
+
+    public static final int OP_WRITE = 1 << 2;
+
+    public static final int OP_CONNECT = 1 << 3;
+
+    public static final int OP_ACCEPT = 1 << 4;
+
     private static void parse(SocketChannel sc) throws IOException, InterruptedException {
-        System.out.println("start 处理客户端请求."+ sc +".." + Thread.currentThread().getName());
-        Thread.sleep(1000);
+        System.out.println("start 处理客户端请求." + sc + ".." + Thread.currentThread().getName());
+        //Thread.sleep(1000);
         ByteBuffer bb = ByteBuffer.allocate(1024);
         StringBuilder sb = new StringBuilder();
-        String username;
-        synchronized (sc) {
-            while (sc.read(bb) > 0) {
-                bb.flip();
-                while (bb.hasRemaining()) {
-                    sb.append((char) bb.get());
-                }
-                bb.clear();
-            }
-            username = sb.toString();
-            //System.out.println(username);
-            if (!username.contains(":")) {
-                SocketAddress sa = sc.getRemoteAddress();
-                nameAddress.put(username, sa);
-                addressChannel.put(sa, sc);
-                System.out.println("进入聊天室的是：[" + username + "]-：地址" + sc.getRemoteAddress());
-                sc.write(UTF8Utils.encode("进入聊天室成功！！！你好" + username));
-            } else {
-                String[] users = username.split(":");
-                if (users.length == 3) {
-                    SocketAddress address = nameAddress.get(users[1]);
-                    if (address != null) {
-                        SocketChannel socketChannel = addressChannel.get(address);
-                        if (socketChannel != null) {
-                            socketChannel.write(UTF8Utils.encode(users[0] + "说:【" + users[2] + "】"));
-                        } else {
-                            sc.write(UTF8Utils.encode("他不在线。。"));
+        String username = "";
 
-                        }
-                    } else {
-                        sc.write(UTF8Utils.encode("他不在线。。"));
-                    }
-                } else {
-                    sc.write(UTF8Utils.encode("输入信息无效。。"));
-                }
+        //对于粘包问题 通过\r\n拆分数据包
+        //对于拆包问题 可以保存这个半包数据和客户端地址对应起来，最后再拼起来
+        while (sc.read(bb) > 0) {
+            bb.flip();
+            //while (bb.hasRemaining()) {
+            //sb.append((char) bb.get());
+            //}
+            username += UTF8Utils.decode(bb);
+
+            bb.clear();
+        }
+        //先读取本次所有的数据，然后在根据分隔符分割
+        char[] reqChars = username.toCharArray();
+        // 在此默认命令大小只有20000
+        char[] onceReq = new char[20000];
+        char[] oldReq = getSocketAddressOldReq(sc);
+        System.arraycopy(oldReq, 0, onceReq, 0, oldReq.length);
+        int count = 0;
+        int index = oldReq.length;
+        for (int i = 0, len = reqChars.length; i < len; i++) {
+            if (reqChars[i] == '\r' && reqChars[i + 1] == '\n'){
+
+                System.out.println("单次请求是：[" + String.valueOf(onceReq) + "]");
+
+                //重置所有
+                ++ count;
+                index = 0;
+                i ++;
+            }else {
+                onceReq[index ++] = reqChars[i];
             }
         }
+        // 如果这次存在多余的半包比如xxx/r/nxx，那么xx就是多余的两个字符
+        if(index != 0){
+            char[] newOldReq = new char[index];
+            System.arraycopy(onceReq,0 ,newOldReq, 0 ,index);
+            updateSocketAddressOldReq(sc, newOldReq);
+        }
+        //username = sb.toString();
+        System.out.println("此次接收到的消息大小为：" + username.length() + "，命令条数：" + count);
+
+        updateSocketAddressReqCount(sc, count);
+        //System.out.println("此次接收到的消息为：" + username);
+        if (!username.contains(":")) {
+            SocketAddress sa = sc.getRemoteAddress();
+            nameAddress.put(username, sa);
+            addressChannel.put(sa, sc);
+            //System.out.println("进入聊天室的是：[" + username + "]-：地址" + sc.getRemoteAddress());
+            sc.write(UTF8Utils.encode("进入聊天室成功！！！你好" + username));
+        } else {
+            String[] users = username.split(":");
+            if (users.length == 3) {
+                SocketAddress address = nameAddress.get(users[1]);
+                if (address != null) {
+                    SocketChannel socketChannel = addressChannel.get(address);
+                    if (socketChannel != null) {
+                        String resp = users[0] + "说:【" + users[2] + "】";
+                        System.out.println("回应信息是:[" + resp + "]");
+                        socketChannel.write(UTF8Utils.encode(resp));
+                    } else {
+                        sc.write(UTF8Utils.encode("他不在线。。"));
+
+                    }
+                } else {
+                    sc.write(UTF8Utils.encode("他不在线。。"));
+                }
+            } else {
+                sc.write(UTF8Utils.encode("输入信息无效。。"));
+            }
+        }
+
         System.out.println("end 处理客户端请求..." + Thread.currentThread().getName());
+
+    }
+
+    private static void updateSocketAddressOldReq(SocketChannel sc, char[] oldReq) {
+        try {
+            SocketAddress sa = sc.getRemoteAddress();
+            addressOldReq.put(sa, oldReq);
+            System.out.println(sa.toString() + "此次有半包======" + String.valueOf(oldReq));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static char[] getSocketAddressOldReq(SocketChannel sc) {
+        char[] oldReq = new char[0];
+        try {
+            SocketAddress sa = sc.getRemoteAddress();
+            if(addressOldReq.get(sc) != null){
+                System.out.println(sa.toString() + "此次存在历史遗留的包======" + String.valueOf(oldReq));
+                oldReq = (char[]) addressOldReq.get(sc);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return oldReq;
+    }
+
+    private static void updateSocketAddressReqCount(SocketChannel sc, int count) {
+        try {
+            SocketAddress sa = sc.getRemoteAddress();
+            Integer oldCount = addressReqCount.get(sa);
+            addressReqCount.put(sa, oldCount == null ? count : oldCount + count);
+            System.out.println(sa.toString() + "当前已经请求了======" + addressReqCount.get(sa) +"次！！！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
