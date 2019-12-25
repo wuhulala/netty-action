b26ea7994d491bf144374329ae5419188f3548b3
wuhulala <370031044@qq.com>
2019-12-25 20:42:47 +0800

    :tada 添加序列化组件

---
+++ chapter7-rpc/rpc-serialization/rpc-serialization-api/src/main/java/com/wuhulala/rpc/serialzation/Serialization.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wuhulala.rpc.serialzation;
+
+import com.alibaba.cooma.Extension;
+import com.wuhulala.rpc.bean.RpcDesc;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Serialization strategy interface that specifies a serializer. (SPI, Singleton, ThreadSafe)
+ *
+ * The default extension is hessian2 and the default serialization implementation of the dubbo protocol.
+ * <pre>
+ *     e.g. &lt;dubbo:protocol serialization="xxx" /&gt;
+ * </pre>
+ */
+@Extension
+public interface Serialization {
+
+    /**
+     * Get content type unique id, recommended that custom implementations use values greater than 20.
+     *
+     * @return content type id
+     */
+    byte getContentTypeId();
+
+    /**
+     * Get content type
+     *
+     * @return content type
+     */
+    String getContentType();
+
+    /**
+     * Get a serialization implementation instance
+     *
+     * @param url URL address for the remote service
+     * @param output the underlying output stream
+     * @return serializer
+     * @throws IOException
+     */
+    ObjectOutput serialize(RpcDesc url, OutputStream output) throws IOException;
+
+    /**
+     * Get a deserialization implementation instance
+     *
+     * @param url URL address for the remote service
+     * @param input the underlying input stream
+     * @return deserializer
+     * @throws IOException
+     */
+    ObjectInput deserialize(RpcDesc url, InputStream input) throws IOException;
+
+}
